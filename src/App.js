e9a6928c458d157929707869cb3941782c6d85e4
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-10-05 17:51:02 +0300

    project_structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './pages/Main'
+import { useState } from "react";
+function App() {
+  
+
+  return(
+    <Main/>
+  );
+}
+
+export default App;
